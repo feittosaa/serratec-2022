0b353f7f46ccd7ea8813856a541e5ce029075494
feittosaa <lucas.feitosa2004@gmail.com>
2022-06-14 17:44:56 -0300

    .

---
+++ react/14-06/estudo1/my-app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import List from './components/List';
+
+function App() {
+  const name = "Lucas"
+  const nameUp = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg"
+
+  return (
+    <div className="App">
+      <h1>Hello, {nameUp}</h1>
+      <p>my first app</p>
+      <p>Soma: {sum(1, 4)}</p>
+      <img src={url} alt="chrome"></img>
+
+      <HelloWorld />
+      <SayMyName name="Matheus" />
+      <SayMyName name="Joao" />
+      <SayMyName name={name} />
+
+      <Pessoa
+        idade="26"
+        profissao="Programador"
+        nome="Rodrigo"
+        foto="https://muradpma.com/wp-content/uploads/2021/10/Alexandre-Fragoso-Machado-300x300.jpg" />
+
+        <List/>
+    </div>
+  );
+}
+
+export default App;
