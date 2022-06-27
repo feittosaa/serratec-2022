a31d5dc49a8fdcac4494855b43a219ace75e366d
feittosaa <lucas.feitosa2004@gmail.com>
2022-06-27 19:05:11 -0300

    .

---
+++ react/native/aula1/src/components/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Image, Text, TouchableOpacity } from "react-native";
+
+export const Button = () => {
+    return (
+        <TouchableOpacity>
+
+        </TouchableOpacity>
+    )
+}
